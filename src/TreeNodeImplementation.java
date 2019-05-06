dba9ab4cfc802ed3febaa2aee66b71916c431faf
MenglinYu <yml.100205@gmail.com>
2019-05-06 09:49:56 -0400

    TreeNodeImplementation_ initial commit

---
+++ src/TreeNodeImplementation.java
@@ -0,0 +1,16 @@
+public class TreeNodeImplementation {
+    public static void main(String[] args) {
+
+    }
+}
+
+class TreeNode {
+    int value;
+    TreeNode left；
+    TreeNode right；
+
+    public TreeNode(int v) {
+        value = v;
+    }
+}
+
