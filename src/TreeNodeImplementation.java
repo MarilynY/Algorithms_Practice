60c5d4a19f2285b6dee762725ee46b299eca6263
Marilyn <yml.100205@gmail.com>
2019-05-07 18:13:56 -0400

    TreeNodeImplementation

---
+++ src/TreeNodeImplementation.java
@@ -0,0 +1,16 @@
+public class TreeNodeImplementation {
+    public static void main(String[] args) {
+
+    }
+}
+
+class TreeNode {
+    int key;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int v) {
+        key = v;
+    }
+}
+
