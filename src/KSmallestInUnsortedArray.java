a78d09ff184c0624ed71bd8dd548e67186f77f89
MenglinYu <yml.100205@gmail.com>
2019-05-08 13:26:55 -0400

    KSmallestInUnsortedArray_bug fixed

--- src/KSmallestInUnsortedArray.java
+++ src/KSmallestInUnsortedArray.java
@@ -6,7 +6,7 @@ import java.util.Queue;
 public class KSmallestInUnsortedArray {
     public static void main(String[] args) {
         int[] array = {1, 3, 9, 0, 2, 5, 5};
-        int k = 6;
+        int k = 0;
         int[] res = kSmallest(array, k);
         for (int x : res) {
             System.out.print(x + " ");
@@ -14,11 +14,16 @@ public class KSmallestInUnsortedArray {
     }
 
     public static int[] kSmallest(int[] array, int k) {
-        Queue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
         int[] result = new int[k];
+        //corner case
+        if (k == 0) {
+            return result;
+        }
+
+        Queue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
         //pretent to heapify
         for (int i = 0; i < k; i++) {
-            maxHeap.offer(array[i]);
+            maxHeap.offer(array[i]);       //if no corner case check, here will throw NPE
         }
         //iterate the rest of elements in array
         for (int i = k; i < array.length; i++) {
